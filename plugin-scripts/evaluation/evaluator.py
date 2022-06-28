21e5532777c6b0044b9b03a117519584aa180212
Koen-Kraaijveld <h.j.kraaijveld@student.vu.nl>
2022-06-28 23:50:08 +0200

    Added comments to all functions used in the evaluation section

--- plugin-scripts/evaluation/evaluator.py
+++ plugin-scripts/evaluation/evaluator.py
@@ -1,14 +1,25 @@
 import json
 
 from metrics_plotter import graph_concentration_impact, graph_locality, graph_dependency_degree, \
-    graph_adaptive_testabiltiy, graph_coupling, graph_lack_of_cohesion
+    graph_adaptive_complexity, graph_coupling, graph_lack_of_cohesion
 
 
 def evaluate(data, exemplar_name, is_class_based):
+    """
+    Generates all the graphs showing the results from each metric.
+
+    Args:
+        data: the data containing the results from the metrics.
+        exemplar_name: the name of the exemplar to be used in the graph's title and labels.
+        is_class_based: true if the graphs will be generated for metrics that may only run on classes.
+
+    Returns: the data containing the results from the metrics
+    """
+
     graph_locality(data, exemplar_name)
     graph_concentration_impact(data, exemplar_name)
     graph_dependency_degree(data, exemplar_name)
-    graph_adaptive_testabiltiy(data, exemplar_name)
+    graph_adaptive_complexity(data, exemplar_name)
 
     if is_class_based:
         graph_coupling(data, exemplar_name)
@@ -18,20 +29,49 @@ def evaluate(data, exemplar_name, is_class_based):
 
 
 def print_data(data):
+    """
+    Prints the data using JSON.
+
+    Args:
+        data: the data to be printed.
+    """
+
     print(json.dumps(data, indent=3))
 
 
 def save_data(data, file):
+    """
+    Saves the data using JSON.
+
+    Args:
+        data: the data to be saved.
+        file: the file where the data will be saved to.
+    """
+
     with open(file, 'w') as outfile:
         json.dump(data, outfile)
 
 
 def read_data(file):
+    """
+    Reads data from a file using JSON.
+
+    Args:
+        file: the file to read from.
+
+    Returns: the data from the file that has been read.
+    """
+
     with open(file) as json_file:
         return json.load(json_file)
 
 
 def generate_blank_data_template():
+    """
+    Generates an empty data template that will contain all the results from the metrics.
+
+    Returns: an empty data template that will contain all the results from the metrics.
+    """
     return {
         "ConcentrationOfImpact": {
             "Decentralization": {
--- plugin-scripts/evaluation/metrics_plotter.py
+++ plugin-scripts/evaluation/metrics_plotter.py
@@ -1,22 +1,24 @@
-import math
-
 from matplotlib import pyplot as plt
-from matplotlib import ticker
 import numpy as np
+
 params = {'legend.fontsize': 'x-large',
           'figure.figsize': (15, 5),
-         'axes.labelsize': 'x-large',
-         'axes.titlesize':'x-large',
-         'xtick.labelsize':'x-large',
-         'ytick.labelsize':'x-large'}
+          'axes.labelsize': 'x-large',
+          'axes.titlesize': 'x-large',
+          'xtick.labelsize': 'x-large',
+          'ytick.labelsize': 'x-large'}
 plt.rcParams.update(params)
 
 
-def graph_concentration_of_impact_stats(data):
-    pass
-
-
 def graph_locality(data, exemplar_name):
+    """
+    Generates a graph to plot the Impact Radius from an exemplar's JSON data.
+
+    Args:
+        data: the data to be plotted.
+        exemplar_name: the name of the exemplar to be used in the graph's title and labels.
+    """
+
     report = data["Adaptive Metrics Report"]
     ent_names = collect_dict_values_from_key(report, "Ent")
     locality = collect_dict_values_from_key(report, "Locality")
@@ -25,25 +27,43 @@ def graph_locality(data, exemplar_name):
     fig, ax = plt.subplots(1, 1)
     cmap = plt.get_cmap("Paired")(np.arange(len(ent_names)))
 
-    plot_double_bar_chart(ax, ent_names, impact_radius, "Local and Global Impact Radius of each adaptive strategy in " + exemplar_name,
-                          "Number of reachable executable statements", True, cmap[0:2])
+    plot_double_bar_chart(ax, ent_names, impact_radius, "Local and Global Impact Radius of each adaptive strategy in " +
+                          exemplar_name, "Number of reachable executable statements", True, cmap[0:2])
 
     plt.subplots_adjust(top=0.97, bottom=0.3)
     plt.show()
 
 
 def graph_concentration_impact(data, exemplar_name):
+    """
+    Organizes the data to be plotted for the Concentration of Impact metric from an exemplar's JSON data.
+
+    Args:
+        data: the data to be plotted.
+        exemplar_name: the name of the exemplar to be used in the graph's title and labels.
+    """
+
     spread = data["ConcentrationOfImpact"]["Decentralization"]["Spread"]
     if len(spread) == 0:
         return
 
     ent_names = collect_dict_values_from_key(spread, "Ent")
     relative_values = collect_dict_values_from_key(spread, "Relative")
-    absolute_values = collect_dict_values_from_key(spread, "Absolute")
-    generate_coc_pie_chart_and_bar_chart(ent_names, relative_values, absolute_values, 100, 1, exemplar_name)
+    generate_relative_coc_pie_chart(ent_names, relative_values, 100, 1, exemplar_name)
 
 
-def generate_relative_coc_pie_chart(labels, relative_values, absolute_values, text_limit, autopct_limit, exemplar_name):
+def generate_relative_coc_pie_chart(labels, relative_values, text_limit, autopct_limit, exemplar_name):
+    """
+    Generates a pie chart from the organized data for the Concentration of Impact metric.
+
+    Args:
+        labels: the text labels corresponding to the names of the adaptive strategies.
+        relative_values: the Relative Global Impact Radius values to be plotted.
+        text_limit: the text limit to determine when text is placed outside of its pie chart slice.
+        autopct_limit: the percentage to determine when text is placed outside of its pie chart slice.
+        exemplar_name: the name of the exemplar to be used in the graph's title and labels.
+    """
+
     new_labels = []
     for i in range(0, len(relative_values)):
         if relative_values[i] * 100 > text_limit or (
@@ -64,49 +84,19 @@ def generate_relative_coc_pie_chart(labels, relative_values, absolute_values, te
     ax1.axis("off")
     ax1.legend(pie[0], labels, loc="center left")
 
-    # sf2.suptitle("Absolute Global Impact Radius of each adaptive strategy")
-    # ax3 = sf2.subplots(1, 1)
-    # ax3.barh(labels, absolute_values, color=cmap)
-    # ax3.yaxis.set_ticklabels([])
-    # ax3.set_xlabel("Number of reachable executable statements across the entire system")
-
-    plt.subplots_adjust(left=0.3, right=0.7, top=0.9, bottom=0.15)
-    plt.show()
-
-
-def generate_coc_pie_chart_and_bar_chart(labels, relative_values, absolute_values, text_limit, autopct_limit, exemplar_name):
-    new_labels = []
-    for i in range(0, len(relative_values)):
-        if relative_values[i] * 100 > text_limit or (
-                relative_values[i] * 100 < autopct_limit and relative_values[i] > 1 / 1000):
-            new_labels.append(str(round(relative_values[i] * 100, 3)))
-        else:
-            new_labels.append('')
-
-    fig = plt.figure()
-    (sf1, sf2) = fig.subfigures(2, 1)
-    cmap = plt.get_cmap("Dark2")(np.arange(len(labels)))
-
-    sf1.suptitle("Relative Global Impact Radius of each adaptive strategy in " + exemplar_name + " (%)")
-    (ax1, ax2) = sf1.subplots(1, 2)
-    ax2.axis("equal")
-    pie = ax2.pie(relative_values, labels=new_labels, startangle=0, autopct=generate_autopct(autopct_limit),
-                  colors=cmap, wedgeprops={"edgecolor": "w", 'linewidth': 1})
-
-    ax1.axis("off")
-    ax1.legend(pie[0], labels, loc="center left")
-
-    sf2.suptitle("Absolute Global Impact Radius of each adaptive strategy")
-    ax3 = sf2.subplots(1, 1)
-    ax3.barh(labels, absolute_values, color=cmap)
-    ax3.yaxis.set_ticklabels([])
-    ax3.set_xlabel("Number of reachable executable statements across the entire system")
-
     plt.subplots_adjust(left=0.3, right=0.7, top=0.9, bottom=0.15)
     plt.show()
 
 
 def graph_dependency_degree(data, exemplar_name):
+    """
+       Generates a graph to plot the Dependency Degree from an exemplar's JSON data.
+
+       Args:
+           data: the data to be plotted.
+           exemplar_name: the name of the exemplar to be used in the graph's title and labels.
+   """
+
     report = data["Adaptive Metrics Report"]
     ent_names = collect_dict_values_from_key(report, "Ent")
     dependencies = collect_dict_values_from_key(report, "Dependencies")
@@ -117,15 +107,25 @@ def graph_dependency_degree(data, exemplar_name):
 
     cmap = plt.get_cmap("Paired")(np.arange(len(ent_names)))
 
-    plot_double_bar_chart(ax, ent_names, dependencies, "Dependency Degree of each adaptive strategy in " + exemplar_name,
-                          "Number of adaptive strategies that have a \n dependency relation with another adaptive strategy",
+    plot_double_bar_chart(ax, ent_names, dependencies,
+                          "Dependency Degree of each adaptive strategy in " + exemplar_name,
+                          "Number of adaptive strategies that have a \n dependency relation with another adaptive "
+                          "strategy",
                           True, cmap[2:4])
 
     plt.subplots_adjust(top=0.97, bottom=0.3)
     plt.show()
 
 
-def graph_adaptive_testabiltiy(data, exemplar_name):
+def graph_adaptive_complexity(data, exemplar_name):
+    """
+       Generates a graph to plot the Adaptive Complexity from an exemplar's JSON data.
+
+       Args:
+           data: the data to be plotted.
+           exemplar_name: the name of the exemplar to be used in the graph's title and labels.
+   """
+
     report = data["Adaptive Metrics Report"]
     ent_names = collect_dict_values_from_key(report, "Ent")
 
@@ -139,13 +139,21 @@ def graph_adaptive_testabiltiy(data, exemplar_name):
 
     plot_double_bar_chart(ax, ent_names, complexity,
                           "Adaptive Complexity of each adaptive in " + exemplar_name,
-                          "Number of linearly independent paths through \n an adaptive strategy's control flow graph", True, cmap[4:6])
+                          "Number of linearly independent paths through \n an adaptive strategy's control flow graph",
+                          True, cmap[4:6])
 
     plt.subplots_adjust(top=0.97, bottom=0.3)
     plt.show()
 
 
 def graph_adaptive_metrics_report(data):
+    """
+       Generates a series of graphs for each adaptive strategy in the architecture from an exemplar's JSON data.
+
+       Args:
+           data: the data to be plotted.
+    """
+
     report = data["Adaptive Metrics Report"]
     ent_names = collect_dict_values_from_key(report, "Ent")
     locality = collect_dict_values_from_key(report, "Locality")
@@ -177,18 +185,25 @@ def graph_adaptive_metrics_report(data):
 
 
 def plot_double_bar_chart(ax, labels, values, title, y_axis_label, has_xticks, cmap):
+    """
+    Generates a bar chart with two color-coded bars for each adaptive strategy.
+
+    Args:
+        ax: the subplot that the graph will be generated on.
+        labels: the names of the adaptive strategies.
+        values: the resulting values of a metric computed from the adaptive strategies
+        title: the title of the generated graph.
+        y_axis_label: the label used for the y-axis.
+        has_xticks: true if the graph has ticks on the x-axis.
+        cmap: the color map used by the generated graph.
+    """
+
     first_key = [list(d.keys())[0] for d in values][0]
-    # print(first_key)
 
     second_key = [list(d.keys())[1] for d in values][0]
-    # print(second_key)
 
     first_values = [list(d.values())[0] for d in values]
     second_values = [list(d.values())[1] for d in values]
-
-    # print(labels)
-    # print(first_values)
-    # print(second_values)
 
     width = 0.35
     x = np.arange(len(labels))
@@ -212,6 +227,13 @@ def plot_double_bar_chart(ax, labels, values, title, y_axis_label, has_xticks, c
 
 
 def graph_modifiability(data):
+    """
+       Generates a graph to plot the Adaptive Modifiability from an exemplar's JSON data.
+
+       Args:
+           data: the data to be plotted.
+   """
+
     report = data["Adaptive Metrics Report"]
     modifiability = []
     ent_names_with_modifiability = []
@@ -240,6 +262,15 @@ def graph_modifiability(data):
 
 
 def graph_coupling(data, exemplar_name):
+    """
+        Generates a graph to plot the Coupling of an architecture of adaptive strategies from an exemplar's
+        JSON data.
+
+       Args:
+           data: the data to be plotted.
+           exemplar_name: the name of the exemplar to be used in the graph's title and labels.
+   """
+
     report = data["Adaptive Metrics Report"]
     modifiability = []
     ent_names_with_modifiability = []
@@ -254,7 +285,8 @@ def graph_coupling(data, exemplar_name):
     fig.subplots_adjust(hspace=0.5)
 
     coupling = [list(d.values())[0] for d in modifiability]
-    plot_single_bar_chart(ax, ent_names_with_modifiability, coupling, "Coupling of each adaptive strategy in " + exemplar_name,
+    plot_single_bar_chart(ax, ent_names_with_modifiability, coupling,
+                          "Coupling of each adaptive strategy in " + exemplar_name,
                           "Number classes coupled to", True, cmap[6])
 
     plt.subplots_adjust(top=0.97, bottom=0.3)
@@ -262,6 +294,15 @@ def graph_coupling(data, exemplar_name):
 
 
 def graph_lack_of_cohesion(data, exemplar_name):
+    """
+       Generates a graph to plot the Lack of Cohesion of an architecture of adaptive strategies from an exemplar's
+       JSON data.
+
+       Args:
+           data: the data to be plotted.
+           exemplar_name: the name of the exemplar to be used in the graph's title and labels.
+   """
+
     report = data["Adaptive Metrics Report"]
     modifiability = []
     ent_names_with_modifiability = []
@@ -278,13 +319,27 @@ def graph_lack_of_cohesion(data, exemplar_name):
     lack_of_cohesion = [list(d.values())[1] for d in modifiability]
     plot_single_bar_chart(ax, ent_names_with_modifiability, lack_of_cohesion,
                           "Lack of Cohesion of each adaptive strategy in " + exemplar_name,
-                          "Average number of class \n instance variables not \n used by its functions (%)", True, cmap[6])
+                          "Average number of class \n instance variables not \n used by its functions (%)", True,
+                          cmap[6])
 
     plt.subplots_adjust(top=0.97, bottom=0.3)
     plt.show()
 
 
 def plot_single_bar_chart(ax, labels, values, title, y_axis_label, has_xticks, cmap):
+    """
+        Generates a bar chart for the metrics results from each adaptive strategy.
+
+        Args:
+            ax: the subplot that the graph will be generated on.
+            labels: the names of the adaptive strategies.
+            values: the resulting values of a metric computed from the adaptive strategies
+            title: the title of the generated graph.
+            y_axis_label: the label used for the y-axis.
+            has_xticks: true if the graph has ticks on the x-axis.
+            cmap: the color map used by the generated graph.
+        """
+
     width = 0.35
     x = np.arange(len(labels))
     bar = ax.bar(x, values, width, color=cmap)
@@ -301,11 +356,38 @@ def plot_single_bar_chart(ax, labels, values, title, y_axis_label, has_xticks, c
 
 
 def collect_dict_values_from_key(list, key):
+    """
+    Collects values of a key from a list of dictionaries.
+    Args:
+        list: the list of dictionaries.
+        key: the key corresponding to the value that will be collected from each dictionary in the list.
+
+    Returns: a list containing the values from each key from each dictionary.
+    """
+
     return [d[key] for d in list]
 
 
 def generate_autopct(limit):
+    """
+    Wrapper function that is used to generate a limit that is used for the text and percentages of a pie chart.
+
+    Args:
+        limit: defines the limit on which text and percentages will be shown in the pie chart.
+
+    Returns: the limit on which text and percentages will be shown in the pie chart.
+    """
+
     def autopct(percentage):
+        """
+        Generates a string to define the limit for the text and percentages of a pie chart.
+
+        Args:
+            percentage: defines the limit on which text and percentages will be shown in the pie chart.
+
+        Returns: the string defining the limit on which text and percentages will be shown in the pie chart.
+        """
+
         return ('%.2f' % percentage) if percentage > limit else ''
 
     return autopct
